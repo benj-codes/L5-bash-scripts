4bcd45e5e7185d149d03c77c2889753dd6afccf8
benj-codes <83542705+benj-codes@users.noreply.github.com>
2022-11-30 10:24:07 +0000

    Create re-direct.sh

---
+++ re-direct.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+package=xclip
+
+sudo apt install $package >> package_install_results.log
+
+if [ $? -eq 0 }
+
+then
+
+  echo "The installation of $package was successful."
+
+else
+
+  echo "$package failed to install." >> package_install_failure.log
+
+fi
