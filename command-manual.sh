685d62a57b3d2443f4a7f361b013d4f4c898f2b8
benj-codes <83542705+benj-codes@users.noreply.github.com>
2022-12-03 12:11:22 +0000

    Create command-manual.sh

---
+++ command-manual.sh
@@ -0,0 +1,20 @@
+!/bin/bash
+
+echo "Which of these would you like to find out the command for?"
+
+echo "1 - create directories"
+echo "2 - show exact path to the current directory"
+echo "3 - create file"
+echo "4 - delete files and directories"
+echo "5 - navigate and change the working directory you're in"
+
+read command;
+
+case $command in
+        1) echo "mkdir";;
+        2) echo "pwd";;
+        3) echo "touch";;
+        4) echo "rm";;
+        5) echo "cd";;
+        *) echo "invalid option"
+esac
