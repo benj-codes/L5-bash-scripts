6a731d64f1799cf2003b1d504ef20cea54c8a403
benj-codes <83542705+benj-codes@users.noreply.github.com>
2022-12-04 18:36:14 +0000

    added email notification for install failure

--- re-direct.sh
+++ re-direct.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 package=xclip
+recipient="c0011865@hallam.shu.ac.uk"
 
 sudo apt install $package >> package_install_results.log
 
@@ -13,5 +14,6 @@ then
 else
 
   echo "$package failed to install." >> package_install_failure.log
+  echo "SUBJECT: FAILURE, the package failed to install." | sendmail $recipient
 
 fi
