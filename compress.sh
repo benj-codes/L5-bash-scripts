92f1790c219e673d220eb2d21b11c3d94113880c
benj-codes <83542705+benj-codes@users.noreply.github.com>
2022-12-07 21:53:18 +0000

    added comments

--- compress.sh
+++ compress.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# Checking for any file ending in .log inside the logfiles directory
 for file in logfiles/*.log
 do
+# Compresses every file it finds
         tar -czvf $file.tar.gz $file
 done
